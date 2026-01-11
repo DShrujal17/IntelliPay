92abfebdc0cfc63d03a840261a70552191835134
DShrujal17 <shrujaldoshi907@gmail.com>
2026-01-11 21:12:15 +0530

    initialize the intelliPay

---
+++ src/main/java/com/intellipay/IntelliPayApplication.java
@@ -0,0 +1,13 @@
+package com.intellipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliPayApplication.class, args);
+	}
+
+}
